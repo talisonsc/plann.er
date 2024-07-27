5e154e9f57ef34a1925ae5a17e3b753d63b696d3
talisonsc <talisonsilva8544@gmail.com>
2024-07-27 13:57:21 -0300

    trip-datails

--- src/pages/creat-trip/index.tsx
+++ src/pages/creat-trip/index.tsx
@@ -1,17 +1,9 @@
-import {
-  MapPin,
-  Calendar,
-  ArrowRight,
-  UserRoundPlus,
-  Settings2,
-  X,
-  AtSign,
-  Plus,
-  User,
-  Mail,
-} from "lucide-react"
 import { FormEvent, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { InviteGuestsModal } from "./invite-guests-modal"
+import { ConfirmTripModal } from "./confirm-trip-modal"
+import { DestinationAndDateStep } from "./steps/destination-and-date-step"
+import { InviteGuestsStep } from "./steps/invite-guests-step"
 
 export function CreatTripPage() {
   const navigate = useNavigate()
@@ -69,7 +61,8 @@ export function CreatTripPage() {
     setIsConfirmTripModalOpen(false)
   }
 
-  function createTrip() {
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
     navigate("/trips/123")
   }
   return (
@@ -82,71 +75,17 @@ export function CreatTripPage() {
           </p>
         </div>
         <div className="space-y-4">
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <div className="flex items-center gap-2 flex-1">
-              <MapPin className="size-5 text-zinc-400" />
-              <input
-                disabled={isGuestsInputOpen}
-                type="text"
-                placeholder="Para onde você vai?"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              <Calendar className="size-5 text-zinc-400" />
-              <input
-                disabled={isGuestsInputOpen}
-                type="text"
-                placeholder="Quando"
-                className="bg-transparent text-lg  placeholder-zinc-400 w-40 outline-none"
-              />
-            </div>
-            <div className="w-px h-6 bg-zinc-800"></div>
-            {isGuestsInputOpen ? (
-              <button
-                onClick={closeGuestsInput}
-                className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700 "
-              >
-                Altera local/data
-                <Settings2 className="size-5 text-zinc-200" />
-              </button>
-            ) : (
-              <button
-                onClick={openGuestsInput}
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 "
-              >
-                Continuar
-                <ArrowRight className="size-5 text-lime-950" />
-              </button>
-            )}
-          </div>
+          <DestinationAndDateStep
+            closeGuestsInput={closeGuestsInput}
+            openGuestsInput={openGuestsInput}
+            isGuestsInputOpen={isGuestsInputOpen}
+          />
           {isGuestsInputOpen && (
-            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <button
-                type="button"
-                onClick={openGuestsModal}
-                className=" flex items-center gap-2 flex-1 text-left"
-              >
-                <UserRoundPlus className="size-5 text-zinc-400" />
-                {emailsToInvite.length > 0 ? (
-                  <span className="text-zinc-100 text-lg flex-1">
-                    {emailsToInvite.length} pessoa(s) convidada(s)
-                  </span>
-                ) : (
-                  <span className="text-zinc-400 text-lg flex-1">
-                    Quem estará na viagem?
-                  </span>
-                )}
-              </button>
-              <div className="w-px h-6 bg-zinc-800"></div>
-              <button
-                onClick={openConfirmTripModal}
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 "
-              >
-                Confirmar Viagem
-                <ArrowRight className="size-5 text-lime-950" />
-              </button>
-            </div>
+            <InviteGuestsStep
+              emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
+              openGuestsModal={openGuestsModal}
+            />
           )}
         </div>
         <p className="text-sm text-zinc-500">
@@ -163,119 +102,19 @@ export function CreatTripPage() {
         </p>
       </div>
       {isGuestsModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="bg-zinc-900 text-zinc-400 w-[640px] shadow-shape rounded-xl px-5 py-5 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">Selecionar Convidados</h2>
-                <button type="button" onClick={closeGuestsModal}>
-                  <X className="size-5 text-zinc-400" />
-                </button>
-              </div>
-              <p className="text-sm text-zinc-400">
-                Os convidados irão receber e-mails para confirmar a participação
-                na viagem.
-              </p>
-            </div>
-            <div className="flex flex-wrap gap-2">
-              {emailsToInvite.map((email) => {
-                return (
-                  <div
-                    key={email}
-                    className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
-                  >
-                    <span className="text-zinc-300">{email}</span>
-                    <button type="button">
-                      <X
-                        onClick={() => removeEmailFromInvites(email)}
-                        className="size-4 text-zinc-400"
-                      />
-                    </button>
-                  </div>
-                )
-              })}
-            </div>
-            <div className="w-full h-px bg-zinc-800" />
-            <form
-              onSubmit={addNewEmailToInvite}
-              className="p-2.5 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2"
-            >
-              <div className="px-2 flex items-center flex-1 gap-2">
-                <AtSign className="size-5 text-zinc-400" />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Digite o e-mail do convidado"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 "
-              >
-                Convidar
-                <Plus className="size-5 text-lime-950" />
-              </button>
-            </form>
-          </div>
-        </div>
+        <InviteGuestsModal
+          closeGuestsModal={closeGuestsModal}
+          addNewEmailToInvite={addNewEmailToInvite}
+          removeEmailFromInvites={removeEmailFromInvites}
+          emailsToInvite={emailsToInvite}
+        />
       )}
 
       {isConfirmTripModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="bg-zinc-900 text-zinc-400 w-[640px] shadow-shape rounded-xl px-5 py-5 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">
-                  Confirmar criação da viagem
-                </h2>
-                <button type="button" onClick={closeConfirmTripModal}>
-                  <X className="size-5 text-zinc-400" />
-                </button>
-              </div>
-              <p className="text-sm text-zinc-400">
-                Para concluir a criação da viagem para{" "}
-                <span className="text-zinc-100 font-semibold">
-                  Florianópolis, Brasil
-                </span>{" "}
-                nas datas de{" "}
-                <span className="text-zinc-100 font-semibold">
-                  16 a 27 de Agosto de 2024
-                </span>{" "}
-                preencha seus dados abaixo:
-              </p>
-            </div>
-
-            <div className="w-full h-px bg-zinc-800" />
-            <form className="space-y-3">
-              <div className="h-14 bg-zinc-950 border-zinc-800 rounded-lg flex items-center px-2 flex-1 gap-2">
-                <User className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Seu nome completo"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-              <div className="h-14 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 px-2 flex-1 ">
-                <Mail className="size-5 text-zinc-400" />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Seu e-mail pessoal"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-              <button
-                onClick={createTrip}
-                type="submit"
-                className=" w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400 "
-              >
-                Confirmar criação da viagem
-              </button>
-            </form>
-          </div>
-        </div>
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
       )}
     </div>
   )
--- src/pages/trip-datails/index.tsx
+++ src/pages/trip-datails/index.tsx
@@ -1,3 +1,40 @@
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react"
+
 export function TripDatailsPage() {
-  return <h1>Trip Datails</h1>
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto">
+      <div className="px-4 h-16 bg-zinc-900 rounded-xl shadow-shape flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <span className="text-zinc-100 ">Florianópolis, Brasil</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-zinc-100 texts">17 a 23 de Agosto</span>
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700 ">
+            Altera local/data
+            <Settings2 className="size-5 text-zinc-200" />
+          </button>
+        </div>
+      </div>
+      <main className="flex gap-16 py-10">
+        <div className="flex-1">
+          <div className="flex items-center justify-between">
+            <h2 className="font-semibold text-3xl">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-2 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Plus className="size-5 text-lime-950" />
+              Cadastrar Atividade
+            </button>
+          </div>
+        </div>
+        <div className="w-80"></div>
+      </main>
+    </div>
+  )
 }
